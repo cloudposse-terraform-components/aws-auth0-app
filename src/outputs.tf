f0f0e947effccfbd02536b7dd301b9185b2ba8fc
Igor Rodionov <goruha@gmail.com>
2024-11-14 20:18:28 +0100

    Initial commit

--- src/outputs.tf
+++ src/outputs.tf
@@ -1,4 +1,4 @@
-output "mock" {
-  description = "Mock output example for the Cloud Posse Terraform component template"
-  value       = local.enabled ? "hello ${basename(abspath(path.module))}" : ""
+output "auth0_client_id" {
+  value       = auth0_client.this[0].client_id
+  description = "The Auth0 Application Client ID"
 }
